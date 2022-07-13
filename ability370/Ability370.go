bb3b760d392435eb77295197d34443cf901344fc
dingman <anpengpeng163@163.com>
2022-07-13 10:12:17 +0800

    t

--- ability370/Ability370.go
+++ ability370/Ability370.go
@@ -1,12 +1,16 @@
 package ability370
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/Anpengpeng/topsdk"
 	"github.com/Anpengpeng/topsdk/ability370/request"
 	"github.com/Anpengpeng/topsdk/ability370/response"
 	"github.com/Anpengpeng/topsdk/util"
 	"log"
+	"reflect"
+	"strings"
 )
 
 type Ability370 struct {
@@ -30,9 +34,61 @@ func (ability *Ability370) TaobaoTbkDgMaterialOptional(req *request.TaobaoTbkDgM
 		log.Fatal("taobaoTbkDgMaterialOptional error", err)
 		return &respStruct, err
 	}
-	err = util.HandleJsonResponseNew(jsonStr, &respStruct)
+	err = ability.HandleJsonResponseNew(jsonStr, &respStruct)
 	if respStruct.Body == "" || len(respStruct.Body) == 0 {
 		respStruct.Body = jsonStr
 	}
 	return &respStruct, err
+}
+
+func (ability *Ability370) HandleJsonResponseNew(jsonStr string, v interface{}) (err error) {
+
+	if strings.Contains(jsonStr[0:20], "error_response") {
+		err := &util.TopApiRequestError{}
+		jsonStr = jsonStr[18 : len(jsonStr)-1]
+		err2 := json.Unmarshal([]byte(jsonStr), err)
+		if err2 != nil {
+			return err2
+		}
+		return err
+	}
+	var res map[string]interface{}
+	_ = json.Unmarshal([]byte(jsonStr), &res)
+	for firKey, v := range res {
+		if reflect.TypeOf(v).Kind().String() == "slice" {
+			newSlice := []interface{}{}
+			for _, value := range v.([]interface{}) {
+				if reflect.TypeOf(value).Kind().String() == "map" {
+					var newMap = make(map[string]interface{})
+					for key, value := range v.(map[string]interface{}) {
+						s := reflect.TypeOf(value).Kind().String()
+						if (key == "item_id" || key == "num_iid") && (s == "int64" || s == "float64") {
+							newMap[key] = fmt.Sprintf("%d", value)
+						} else {
+							newMap[key] = value
+						}
+					}
+					newSlice = append(newSlice, newMap)
+				} else {
+					newSlice = append(newSlice, value)
+				}
+			}
+			res[firKey] = newSlice
+		} else if reflect.TypeOf(v).Kind().String() == "map" {
+			var m = make(map[string]interface{})
+			for key, value := range v.(map[string]interface{}) {
+				s := reflect.TypeOf(value).Kind().String()
+				if (key == "item_id" || key == "num_iid") && (s == "int64" || s == "float64") {
+					m[key] = fmt.Sprintf("%d", value)
+				} else {
+					m[key] = value
+				}
+			}
+			res[firKey] = m
+		}
+	}
+
+	newJsonStr, _ := json.Marshal(res)
+
+	return json.Unmarshal(newJsonStr, v)
 }
